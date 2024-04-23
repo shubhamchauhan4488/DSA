5471cf6ff80accaaab662ee8e071f8b889814ec5
SHUBHAM CHAUHAN <shubhamchauhan4488@gmail.com>
2024-04-23 03:59:27 +0000

    LinkedList search

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -22,7 +22,10 @@ class ListNode {
 
 public class LinkedList {
 
-	/* converts an array to link list */
+	/* converts an array to link list 
+	 *  TC: O(n) worst case
+	 *  SC: O(1) no extra space
+	*/
 	private static ListNode convertArr2LL(int[] arr){
 		// creating the first element of LL
 		ListNode head = new ListNode(arr[0]);
@@ -40,7 +43,10 @@ public class LinkedList {
 		return head;
   }
 
-	/** Traverse the entire linked list */
+	/** Traverse the entire linked list 
+	 *  TC: O(n) worst case
+	 *  SC: O(1) no extra space
+	*/
 	private static void traverse(ListNode head){
 		ListNode current = head;
 		// i.e until we reach the last element, keep going
@@ -50,10 +56,35 @@ public class LinkedList {
 		}
 	}
 
+	/** Searches for 'val' inside the linked list 
+	 *  Returns 1 if found, 0 if not found
+	 *  TC: O(n) worst case
+	 *  SC: O(1) no extra space
+	*/
+	private static int search(ListNode head, int k){
+		// in every link list need to check for empty list
+		if(head == null) return 0;
+		ListNode current = head;
+
+		while(current != null){
+			if(current.data == val){
+				// return wherever the val is found
+				return 1;
+			}
+			current = current.next;
+		}
+		// reached end, so not found
+		return 0;
+	}
+
 	public static void main(String args[]) {
+		System.out.println("-------- Array to List --------");
 		int[] arr = { 12, 5, 8, 4 };
 		ListNode head = convertArr2LL(arr);
 		// System.out.println(head.data);
+		System.out.println("-------- Traverse --------");
 		traverse(head);
+		System.out.println("-------- Search --------");
+		System.out.println(search(head, 8));
 	}
 }
