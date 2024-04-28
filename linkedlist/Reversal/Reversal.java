3e4840656204b6fca7f85c86c6105cfad9776a07
SHUBHAM CHAUHAN <shubhamchauhan4488@gmail.com>
2024-04-28 03:59:31 +0000

    LinkedList: palindrome

--- linkedlist/Reversal/Reversal.java
+++ linkedlist/Reversal/Reversal.java
@@ -13,4 +13,23 @@ class LLDeletion extends LinkedList {
         }
         return prev;
     }
+
+    public static boolean isPalindrome(Node head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+        Node originalHead = head;
+        Node revList =  reverseLL(head);
+        Node temp = revList;
+        while(temp != null && originalHead != null){
+            // System.out.println("head.data" + originalHead.data);
+            // System.out.println("temp.data" + temp.data);
+            if(originalHead.data != temp.data){
+                return false;
+            }
+            originalHead = originalHead.next;
+            temp = temp.next;
+        }
+        return true;
+    }
 }
