d7cb96dddff9f787eaf119b8e6171a5816783d82
SHUBHAM CHAUHAN <shubhamchauhan4488@gmail.com>
2024-05-14 01:48:18 +0000

    update LinkedList deletion

--- linkedlist/Deletion/LLDeletion.java
+++ linkedlist/Deletion/LLDeletion.java
@@ -2,31 +2,64 @@ import linkedlist.LinkedList;
 
 class LLDeletion extends LinkedList {
 
-	/** Deletes the element at the head of the LL
-	 *  TC: O(1), SC: O(1)
+	/**
+	 * Deletes the element at the head of the LL
+	 * TC: O(1), SC: O(1)
 	 */
-	public static ListNode removeNodeAtHead(ListNode head){
-		if(head == null) return head;
+	public static ListNode removeNodeAtHead(ListNode head) {
+		if (head == null)
+			return head;
 		head = head.next;
 		return head;
 	}
 
-	/** Deletes the element at the tail of the LL
-	 *  TC: O(N), SC: O(1)
+	/**
+	 * Deletes the element at the tail of the LL
+	 * TC: O(N), SC: O(1)
 	 */
-	public static ListNode removeNodeAtTail(ListNode head){
-		if(head == null) return head;
+	public static ListNode removeNodeAtTail(ListNode head) {
+		if (head == null)
+			return head;
 		ListNode current = head;
-		while(current.next.next != null){ // second last element
+		while (current.next.next != null) { // second last element
 			current = current.next;
 		}
 		current.next = null; // marking second last node's next as null and hence the last node in the LL
 		return head;
 	}
 
-	/** Deletes the 'k'th element from the end 
-	 *  TC: O(n) SC: O(1) no extra space being used
-	*/
+	/**
+	 * Deletes the element from the middle
+	 * TC: O(n) SC: O(1) no extra space being used
+	 */
+	public static ListNode removeNodeFromMiddle(ListNode head) {
+		if (head == null)
+			return head;
+		// using the fast and the slow pointer approach
+		ListNode fast = head;
+
+		if (fast == null)
+			return head.next;
+
+		fast = fast.next; // moving one ahead so that slow stops at middle -1
+
+		// starting a slow pointer from the start/head
+		ListNode slow = head;
+
+		while (fast.next != null) { // taking fast to the end, incrementing both fast and slow by 1
+			slow = slow.next;
+			fast = fast.next.next; // fast has 2x speed
+		}
+
+		// now perform the deletion, slow will be at the middle - 1
+		slow.next = slow.next.next; // by-pass the middle node
+		return head;
+	}
+
+	/**
+	 * Deletes the 'k'th element from the end
+	 * TC: O(n) SC: O(1) no extra space being used
+	 */
 	public static ListNode removeKthNodeFromEnd(ListNode head, int K) {
 		if (head == null)
 			return head;
@@ -63,11 +96,101 @@ class LLDeletion extends LinkedList {
 		return head;
 	}
 
-	// Traverse after deletion, from the head that is received after making the changes
+	/**
+	 * Deletes the 'k'th element from the start
+	 * TC: O(n) SC: O(1) no extra space being used
+	 */
+	public static ListNode removeKthFromStart(ListNode head, int k) {
+		if (k <= 0) { // invalid
+			return head;
+		}
+		if (k == 1) { // remove first element
+			head = head.next;
+			return head;
+		}
+
+		// the actual kth removal
+		ListNode current = head;
+		ListNode previous = null;
+		int count = 1;
+		while (current.next != null && count < k) { // count 1, count 2, k = 3rd position
+			previous = current;
+			// System.out.println("current" + current.val);
+			current = current.next;
+			count++;
+		}
+		// previous will be at k - 1
+		// [12]->[5](prev)->[8](current)->[4] and we want to delete 8
+
+		// just bypass the [8]
+		previous.next = current.next;
+		return head;
+	}
+
+	public static void removeNodeWithValue(ListNode node) { // just node to be deleted is given, NO ACCESS TO HEAD
+        if(node == null || node.next == null){
+            System.out.println("cannot delete last node with this method");
+        }
+
+        // copy the data from the next node into the current node, thats it!!
+        // the val
+        // the pointer
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+	// Given the head of a linked list and an integer val,
+	// remove ALL the nodes of the linked list that has Node.val == val, and return the new head.
+	/**
+	 * Input: head = [1,2,6,3,4,5,6], val = 6
+		Output: [1,2,3,4,5]
+		Example 2:
+
+		Input: head = [], val = 1
+		Output: []
+		Example 3:
+
+		Input: head = [7,7,7,7], val = 7
+		Output: []
+	 */
+    public static ListNode removeAllElements(ListNode head, int val) {
+        if(head == null) return null;
+		
+        // first element itself, to be deleted
+        if(head.val == val){
+            return removeElements(head.next, val); // just move head + 1 and call the function again
+        } 
+        ListNode current = head;
+        ListNode previous = null;
+        while(current != null){
+            if(current.val == val){
+                previous.next = current.next;
+            }else{
+                previous = current; // maintains the previous node
+            }
+            current = current.next;
+          
+        }
+        return head;
+    }
+	// Traverse after deletion, from the head that is received after making the
+	// changes
 	// System.out.println("-------- Delete Head --------");
-  // traverse(removeNodeAtHead(head));
-  // System.out.println("-------- Delete Tail --------");
+	// traverse(removeNodeAtHead(head));
+	
+	// System.out.println("-------- Delete Tail --------");
 	// traverse(removeNodeAtTail(head));
+
 	// System.out.println("-------- Delete Kth from end --------");
-  // traverse(removeKthNodeFromEnd(head, 2));
+	// traverse(removeKthNodeFromEnd(head, 2));
+
+	// System.out.println("-------- Delete Kth from start --------");
+	// traverse(removeKthFromStart(head, 2));
+
+	// System.out.println("-------- Delete from middle --------");
+	// head = removeNodeFromMiddle(head);
+
+	// System.out.println("-------- Delete a particular Node --------");
+	// removeNodeWithValue(head.next.next); // want to delete 8, passing only that node
 }
